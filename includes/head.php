ae4173e205e70366920149e228cc819b3aab8070
kico0304 <kico_jajcanin@hotmail.com>
2022-04-22 21:56:07 +0200

    slider fix

--- includes/head.php
+++ includes/head.php
@@ -19,8 +19,8 @@
   <!-- <script src="plugins/counterup/jquery.easing.js"></script> -->
   <script src="plugins/counterup/jquery.waypoints.min.js"></script>
   <!-- Slick Slider  CSS -->
-  <!-- <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css"> -->
-  <!-- <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css"> -->
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css">
   <!-- Main Stylesheet -->
   <link rel="stylesheet" href="css/style.css">
 </head>
