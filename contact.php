5325cf5c47b552aa14dc082cea13b87b1511788f
milanr1987 <ratkovicmilangit@gmail.com>
2022-04-22 18:53:06 +0200

    milan

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 
     <!-- HEADER -->
     <?php include 'includes/head.php'; ?>
-    
+
 <body>
     <!-- HEADER -->
     <?php include 'includes/header.php'; ?>
@@ -15,7 +15,7 @@
         <div class="block text-center">
 
           <h1 class="text-capitalize text-lg about-headline">Kontakt</h1>
-          <span class="text-white">Sigma2 - Kontaktirajte nas</span>
+          <span class="text-white">Sigma2 - Potražite nas, pozovite ili pišite</span>
         </div>
       </div>
     </div>
@@ -25,27 +25,28 @@
 <section class="section contact-info pb-0">
     <div class="container">
          <div class="row">
+         <div class="col-lg-4 col-sm-6 col-md-6">
+                <div class="contact-block mb-4 mb-lg-0">
+                    <i class="icofont-location-pin"></i>
+                    <h5>Lokacija</h5>
+                     North Main Street,Brooklyn Australia
+                </div>
+            </div>
             <div class="col-lg-4 col-sm-6 col-md-6">
                 <div class="contact-block mb-4 mb-lg-0">
                     <i class="icofont-live-support"></i>
-                    <h5>Call Us</h5>
+                    <h5>Pozovite nas</h5>
                      +823-4565-13456
                 </div>
             </div>
             <div class="col-lg-4 col-sm-6 col-md-6">
                 <div class="contact-block mb-4 mb-lg-0">
                     <i class="icofont-support-faq"></i>
-                    <h5>Email Us</h5>
+                    <h5>Email</h5>
                      contact@mail.com
                 </div>
             </div>
-            <div class="col-lg-4 col-sm-6 col-md-6">
-                <div class="contact-block mb-4 mb-lg-0">
-                    <i class="icofont-location-pin"></i>
-                    <h5>Location</h5>
-                     North Main Street,Brooklyn Australia
-                </div>
-            </div>
+
         </div>
     </div>
 </section>
@@ -68,7 +69,7 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="alert alert-success contact__msg" style="display: none" role="alert">
-                                Your message was sent successfully.
+                               Vaša poruka je uspješno poslata
                             </div>
                         </div>
                     </div>
@@ -76,33 +77,33 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="form-group">
-                                <input name="name" id="name" type="text" class="form-control" placeholder="Your Full Name" >
+                                <input name="name" id="name" type="text" class="form-control" placeholder="Ime i prezime" >
                             </div>
                         </div>
 
                         <div class="col-lg-6">
                             <div class="form-group">
-                                <input name="email" id="email" type="email" class="form-control" placeholder="Your Email Address">
+                                <input name="email" id="email" type="email" class="form-control" placeholder="E-mail">
                             </div>
                         </div>
                          <div class="col-lg-6">
                             <div class="form-group">
-                                <input name="subject" id="subject" type="text" class="form-control" placeholder="Your Query Topic">
+                                <input name="subject" id="subject" type="text" class="form-control" placeholder="Naslov poruke">
                             </div>
                         </div>
                          <div class="col-lg-6">
                             <div class="form-group">
-                                <input name="phone" id="phone" type="text" class="form-control" placeholder="Your Phone Number">
+                                <input name="phone" id="phone" type="text" class="form-control" placeholder="Telefon">
                             </div>
                         </div>
                     </div>
 
                     <div class="form-group-2 mb-4">
-                        <textarea name="message" id="message" class="form-control" rows="8" placeholder="Your Message"></textarea>
+                        <textarea name="message" id="message" class="form-control" rows="8" placeholder="Poruka"></textarea>
                     </div>
 
                     <div class="text-center">
-                        <input class="btn btn-main btn-round-full" name="submit" type="submit" value="Send Messege"></input>
+                        <input class="btn btn-main btn-round-full" name="submit" type="submit" value="Pošaljite upit"></input>
                     </div>
                 </form>
             </div>
@@ -111,10 +112,10 @@
 </section>
 
 
- <!-- <div class="google-map ">
+ <div class="google-map ">
     <div id="map"></div>
-</div> -->
-<!-- footer Start -->
+</div>
+<!-- footer Start
 
     <!-- FOOTER -->
     <?php include 'includes/footer.php'; ?>
