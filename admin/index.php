dacfabb26ce92538edb7fa91c2b1b965495b58c1
kico0304 <kico_jajcanin@hotmail.com>
2022-04-22 18:14:26 +0200

    admin started

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sigma2 administration panel</title>
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="../plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/style.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ lokacija_.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sigma2 administration panel</title>
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/style.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <a href="index.php" id="adminLogo">CMS</a>
+                <ol>
+                    <li><a href="lokacija.php?id=1">Naslov etaže</a></li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
