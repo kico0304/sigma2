89bb1a219df071f27c339bb2cf1d515b0fc13160
kico0304 <kico_jajcanin@hotmail.com>
2022-05-31 23:14:26 +0200

    test

--- lokacija.php
+++ lokacija.php
@@ -18,6 +18,108 @@
     <?php include 'includes/head.php'; ?>
 
     <body>
+
+        <style>
+
+.parent {
+    display: -ms-flexbox;
+    display: -webkit-box;
+    display: flex;
+}
+
+.row {
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+    -ms-flex-flow: row;
+    flex-flow: row;
+}
+
+.around {
+    -webkit-box-pack: space-around;
+    -ms-flex-pack: space-around;
+    justify-content: space-around;
+}
+
+.h-center {
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+}
+
+.v-center {
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+}
+
+.between {
+    -webkit-box-pack: justify;
+    -ms-flex-pack: justify;
+    justify-content: space-between;
+}
+
+.wrap {
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+}
+
+.clear {
+    clear: both;
+}
+
+.mainWrapper {
+    max-width: 1200px;
+    width: 100%;
+    margin:0 auto;
+}
+
+.mainContainer {
+    width: 100%;
+    height: 850px;
+    background-image: url('../images/Young_City_latvany_10.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+.container {
+    max-width: 450px;
+    width: 100%;
+    margin: 0 10px;
+}
+
+.container-box {
+    background-color: rgba(99,126,137, 0.7);
+    margin: 20px 0;
+    padding: 12px 0;
+    letter-spacing: 0px;
+    width: 100%;
+    opacity: 1;
+    transition: all 0.5s ease-in-out;
+    border-radius: 5px;
+    -webkit-box-shadow: 6px 10px 11px -2px rgba(0,0,0,1);
+    -moz-box-shadow: 6px 10px 11px -2px rgba(0,0,0,1);
+    box-shadow: 6px 10px 11px -2px rgba(0,0,0,1);
+    cursor: pointer;
+}
+
+.container-box:hover {
+    transform: scale(1.1);
+}
+
+.container-box p {
+    color: #fff;
+    font-size: 26px;
+    font-weight: 600;
+}
+
+@media screen and (max-width: 769px) {
+    .mainContainer {
+        height: auto;
+    }
+  }
+
+        </style>
+
     <div class="container">
         <div class="row">
             <!-- INFO O LOKACIJI -->
@@ -30,11 +132,27 @@
             </div>
             <!-- SPRATOVI LOKACIJE -->
             <div class="col-lg-12">
+                <!--Begin mainWrapper-->
+    <div class="mainWrapper">
+        <!--Begin mainContainer-->
+        <div class="mainContainer parent h-center v-center">
+                <!--Begin container-->
+                <div class="container">
                 <?php foreach ($data_ as $spratovi) { ?>
                     <a href="sprat.php?lk=<?php echo $data['lokacija_id'] ?>&id=<?php echo $spratovi['spratovi_id'] ?>">
-                        <h4><?php echo $spratovi['spratovi_naziv'] ?></h4>
+                        <div class="container-box parent h-center v-center">
+                            <p><?php echo $spratovi['spratovi_naziv'] ?></p>
+                        </div>
+                        <h4></h4>
                     </a>
                 <?php } ?>
+                </div>
+                <!--End container-->
+        </div>
+        <!--End mainContainer-->
+    </div>
+    <!--End mainWrapper-->
+                
             </div>
         </div>
     </div>
